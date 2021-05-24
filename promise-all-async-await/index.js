3ea59ed1d4c6bfe5e4ba10b3b82c49954522a2d2
rmunoz <richosojason@msn.com>
2021-05-24 00:10:49 -0500

    added promise index

---
+++ promise-all-async-await/index.js
@@ -0,0 +1,68 @@
+const animals = [
+  { id: 1, name: "cat" },
+  { id: 2, name: "dog" },
+  { id: 3, name: "ferret" },
+  { id: 4, name: "snake" },
+];
+
+const getAnimals = () => {
+  return new Promise((resolve, reject) => {
+    return setTimeout(() => resolve(animals), 1000);
+  });
+};
+
+const getAnimalName = (animal) => {
+  return new Promise((resolve, reject) => {
+    return setTimeout(() => resolve(animal.name), 500);
+  });
+};
+
+const capitalize = (name) => {
+  return new Promise((resolve, reject) => {
+    return setTimeout(() => resolve(name.toUpperCase()), 1500);
+  });
+};
+
+const runAsync = async () => {
+  const animals = await getAnimals();
+  console.log(animals);
+
+  Promise.all(
+    animals.map(async (animal) => {
+      const animalName = await getAnimalName(animal);
+      console.log(animalName);
+
+      const capitalizedName = await capitalize(animalName);
+      console.log(capitalizedName);
+    })
+  );
+};
+
+runAsync();
+
+// Promise.all(
+//   animals.map(async (animal) => {
+//     const animalName = await getAnimalName(animal);
+//     console.log(animalName);
+
+//     const capitalizedName = await capitalize(animalName);
+//     console.log(capitalizedName);
+//   })
+// );
+
+// for (const animal of animals) {
+//   const animalName = await getAnimalName(animal);
+//   console.log(animalName);
+
+//   const capitalizedName = await capitalize(animalName);
+//   console.log(capitalizedName);
+// }
+
+// animals.forEach(async (animal) => {
+//   const animalName = await getAnimalName(animal);
+//   console.log(animalName);
+
+//   const capitalizedName = await capitalize(animalName);
+//   console.log(capitalizedName);
+// });
+// console.log(promiseAll);
