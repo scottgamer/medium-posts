6647b3c358d20a702caf6d635ccf02b23b2670f8
rmunoz <richosojason@msn.com>
2021-11-21 23:51:58 -0500

    added comments fetch method

--- msw-react/src/components/Posts/PostItem.tsx
+++ msw-react/src/components/Posts/PostItem.tsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./PostItem.module.css";
 
 export interface Post {
-  id?: number;
+  id: number;
   title: string;
   body: string;
 }
 
-const PostItem = ({ title, body }: Post): JSX.Element => {
+export interface Comment {
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+const PostItem = ({ post }: { post: Post }): JSX.Element => {
+  const { id, body, title } = post;
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [toggleComments, setToggleComments] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchComments = async (postId: number) => {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+      );
+      const data = await response.json();
+      setComments(data);
+    };
+    fetchComments(id);
+  }, [id]);
+
   return (
     <article className={styles.postItem}>
       <h3>{title}</h3>
       <div>{body}</div>
+      <button onClick={() => setToggleComments(!toggleComments)}>
+        See comments &darr;
+      </button>
+      {toggleComments && comments ? (
+        <div>
+          the comments
+          {comments.map((comment) => (
+            <div key={comment.id}>
+              {comment.body} posted by: {comment.email}
+            </div>
+          ))}
+        </div>
+      ) : null}
     </article>
   );
 };
