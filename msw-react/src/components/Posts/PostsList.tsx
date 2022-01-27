0b0d2029f0bc4d26f429bbd42e9240e9597c3761
rmunoz <richosojason@msn.com>
2022-01-27 13:14:24 -0500

    added conditions to render posts

--- msw-react/src/components/Posts/PostsList.tsx
+++ msw-react/src/components/Posts/PostsList.tsx
@@ -4,15 +4,32 @@ import PostItem, { Post } from "./PostItem";
 
 const PostsList = (): JSX.Element => {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`${CONSTANTS.API_URL}/posts`);
-      const posts = await response.json();
-      setPosts(posts);
+      try {
+        setLoading(true);
+        const response = await fetch(`${CONSTANTS.API_URL}/posts`);
+        const posts = await response.json();
+
+        setPosts(posts);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchPosts();
   }, []);
+
+  if (loading) {
+    return <div>Loading posts...</div>;
+  }
+
+  if (posts.length === 0) {
+    return <div>No posts published</div>;
+  }
 
   return (
     <ul>
