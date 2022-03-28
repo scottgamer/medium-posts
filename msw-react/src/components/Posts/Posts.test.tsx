16dfe5886d9d45e0732116dac5ae8077f9fabe90
rmunoz <richosojason@msn.com>
2022-03-28 12:27:00 -0500

    added post test suite

---
+++ msw-react/src/components/Posts/Posts.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Posts from "./Posts";
+
+describe("Posts component", () => {
+  test("Renders the component without data", () => {
+    render(<Posts />);
+    screen.getByRole("heading", { name: `Posting all day!` });
+  });
+
+  test("Renders the component with loading state", ()=>{
+    render(<Posts />);
+
+  });
+});
