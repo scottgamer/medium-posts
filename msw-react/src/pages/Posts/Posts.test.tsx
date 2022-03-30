caa8808b045efe1af4eb012f4d693df8250fcf61
rmunoz <richosojason@msn.com>
2022-03-30 11:19:59 -0500

    added test to list posts

--- msw-react/src/pages/Posts/Posts.test.tsx
+++ msw-react/src/pages/Posts/Posts.test.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Posts from "./Posts";
+import server from "../../mocks/server";
+import { rest } from "msw";
+import CONSTANTS from "../../constants";
 
 describe("Posts component", () => {
-  test("Renders the component with loading state", () => {
+  test("Renders the component with loading state", async () => {
     render(<Posts />);
-    screen.getByText(/Loading posts.../i);
+    await screen.findByText(/Loading posts.../i);
   });
 
-  // test("Renders the component with loading state", () => {
-  //   render(<Posts />);
-  // });
+  test("Renders the component without posts", async () => {
+    server.use(
+      rest.get(`${CONSTANTS.API_URL}/posts`, (req, res, ctx) => {
+        return res(ctx.status(200), ctx.json([]));
+      })
+    );
+    render(<Posts />);
+    await screen.findByText(/No posts published/i);
+  });
+
+  test("Renders the component with posts", async () => {
+    render(<Posts />);
+    const postsItems = await screen.findAllByRole("article");
+    expect(postsItems).toHaveLength(2);
+  });
 });
